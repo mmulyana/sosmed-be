f71bb07a7880b783a480f6598d6c04b428ce7b73
mmulyana <mulyan.t20@gmail.com>
2025-10-21 10:55:14 +0700

    feat: update contract storage

--- internal/store/storage.go
+++ internal/store/storage.go
@@ -14,16 +14,25 @@ type Storage struct {
 	Posts interface {
 		Create(context.Context, *Post) error
 		GetByID(context.Context, int64) (*Post, error)
+		GetAll(context.Context) ([]Post, error)
 	}
 
 	Users interface {
 		Create(context.Context, *User) error
 	}
+
+	Comments interface {
+		Create(context.Context, *Comment) error
+		GetByPostId(context.Context, int64) ([]Comment, error)
+	}
 }
 
 func NewStorage(db *sql.DB) Storage {
+	comments := &CommentStore{db}
+
 	return Storage{
-		Posts: &PostStore{db},
-		Users: &UserStore{db},
+		Posts:    &PostStore{db: db, commentStore: comments},
+		Users:    &UserStore{db},
+		Comments: comments,
 	}
 }
